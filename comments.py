c8cab99da5503a975c5246b99d2173667c421846
pawarapyarelal1024-dev <pawarapyarelal1024@gmail.com>
2026-03-27 12:40:38 +0530

    Add comments and print statements in comments.py

---
+++ comments.py
@@ -0,0 +1,14 @@
+'''
+hey harry dont remove this line
+author :harry
+this is 100 days of code
+''' 
+print("hey im is \" good boy\"\nand viewer is also a good boy") #escape sequence \""
+
+print("hello world") #printing hello world!
+
+#this single line comments
+
+print("this is print the statements")
+print("hey",6,7,sep="~", end="009\n")
+print("harry")
